653d6af148778f07d9ee82ab2e3c2e8ac63248c9
Jaontolt <jaontolt@leoniscinema.com>
2015-03-25 17:41:00 +0800

    增加ATMOS CONFIG FILE PARSE； 删除不要的文件smtp.cpp/qmsdb.h/qmsdb.cpp； 更改工程文件，加入QtXml引用

--- SQMSServer/Device.h
+++ SQMSServer/Device.h
@@ -3,15 +3,30 @@
 
 #include "DeviceStruct.h"
 #include <QString>
+#include <QStringList>
 
 class IDevice{
 public:
 	virtual QString Read(DATACLASS dataClass) = 0;
 	virtual int Write(DATACLASS dataClass, const QString& strParam) = 0;
 	virtual int SetIp(QString strIp, int nPort = 10001) = 0;
+	virtual DEVICETYPE IdType() = 0;
 };
 
 IDevice* CreateDevice(DEVICETYPE type);
 void ReleaseDevice(IDevice *pDevice);
+
+//--------------------------------------------------------------------------
+//This function just use for parse atmos config file.
+//Usage:
+//       QStringList list = Dolby850ParseConfig(pDev->Read(CP850_GETCONFIG);
+//Return:
+//       All speaker channel name will be save in QStringList
+//		 Please note that the position is very important.
+//		 Such as list[0] = "L", if you want to make it output pinknoise, 
+//		 you need to convert position to QString and pass it to IDevice
+//		 pDev->Write(CP850_SETCHANNEL, QString::number(0));
+QStringList Dolby850ParseConfig(const QString& config);
+//--------------------------------------------------------------------------
 
 #endif
