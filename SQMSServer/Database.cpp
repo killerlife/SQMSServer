c679196172706f789c1cfa7b581add87565e0455
Jaontolt <jaontolt@leoniscinema.com>
2015-03-25 11:56:05 +0800

    补充SQMSServer架构设计，版本为V1.0.2 新增报表数据预警

--- SQMSServer/Database.cpp
+++ SQMSServer/Database.cpp
@@ -27,7 +27,7 @@ private:
 	IConfig *pConfig;
 	ILog *pLog;
 	QSqlDatabase db;
-	QSqlQueryModel model;
+	QSqlQueryModel *pModel;
 	SMTPSETTING mSS;
 	volatile char nFlag; 
 };
@@ -41,7 +41,6 @@ IDatabase* CreateDatabase()
 
 void ReleaseDatabase(IDatabase *pDatabase)
 {
-
 }
 
 SQMSDatabase::SQMSDatabase():nFlag(0)
@@ -108,24 +107,27 @@ SMTPSETTING SQMSDatabase::GetSmtpSetting()
 	if (db.isOpen())
 	{
 		QString sql = QObject::tr("select SenderId, SenderPwd, SmtpServer, ServerPort, RecvList, SendTime from smtpreportconfig");
-		model.setQuery(sql, db);
-		if(model.lastError().isValid())
+		pModel = new QSqlQueryModel;
+		pModel->setQuery(sql, db);
+		if(pModel->lastError().isValid())
 		{
 			pLog->Write(LOG_DATABASE, QObject::tr("Execute GetSmtpSetting SQL failure."));
 		}
-		if(model.rowCount() == 0)
+		if(pModel->rowCount() == 0)
 		{
 			pLog->Write(LOG_DATABASE, QObject::tr("No SMTP Setting in database."));
 		}
 		else
 		{
-			mSS.strSender = model.data(model.index(0, 0)).toString();
-			mSS.strPwd = model.data(model.index(0, 1)).toString();
-			mSS.strServer = model.data(model.index(0, 2)).toString();
-			mSS.nPort = model.data(model.index(0,3)).toInt();
-			mSS.strRecvList = model.data(model.index(0,4)).toString();
-			mSS.mStartTime = model.data(model.index(0, 5)).toTime();
+			mSS.strSender = pModel->data(pModel->index(0, 0)).toString();
+			mSS.strPwd = pModel->data(pModel->index(0, 1)).toString();
+			mSS.strServer = pModel->data(pModel->index(0, 2)).toString();
+			mSS.nPort = pModel->data(pModel->index(0,3)).toInt();
+			mSS.strRecvList = pModel->data(pModel->index(0,4)).toString();
+			mSS.mStartTime = pModel->data(pModel->index(0, 5)).toTime();
 		}
+		delete pModel;
+		pModel = NULL;
 		CloseDB();
 	}
 	nFlag = 0;
@@ -141,39 +143,42 @@ void SQMSDatabase::GetQDevList(TQDevItemList& itemList)
 	if (db.isOpen())
 	{
 		QString sql = QObject::tr("select cinema_hall, dev_ip, fix_ftl, fix_lc, fix_rc, fix_cc, fix_hb, fix_la, fix_ra, start_datetime, end_datetime, cinema_name, script, sync_mode, port from tb_dev_corrected_value_conf ");
-		model.setQuery(sql, db);
-		if (model.lastError().isValid())
+		pModel = new QSqlQueryModel;
+		pModel->setQuery(sql, db);
+		if (pModel->lastError().isValid())
 		{
 			pLog->Write(LOG_DATABASE, QObject::tr("Execute GetQDevList SQL failure."));
+			delete pModel;
+			pModel = NULL;
 			CloseDB();
 			nFlag = 0;
 			return;
 		}
 		itemList.clear();
 		QMSDEVICE item;
-		for(int i = 0; i < model.rowCount(); i++)
+		for(int i = 0; i < pModel->rowCount(); i++)
 		{
-			item.strTheaterNo = model.data(model.index(i, 0)).toString();
-			item.strIp = model.data(model.index(i, 1)).toString();
-			if (model.data(model.index(i, 14)).toInt() == 0)
+			item.strTheaterNo = pModel->data(pModel->index(i, 0)).toString();
+			item.strIp = pModel->data(pModel->index(i, 1)).toString();
+			if (pModel->data(pModel->index(i, 14)).toInt() == 0)
 				item.nPort = 10001;
 			else
-				item.nPort = model.data(model.index(i, 14)).toInt();
+				item.nPort = pModel->data(pModel->index(i, 14)).toInt();
 
 			item.mDevType = DEV_LLAS100;
-			item.fix_ftl = model.data(model.index(i, 2)).toDouble();
-			item.fix_lc = model.data(model.index(i, 3)).toDouble();
-			item.fix_rc = model.data(model.index(i, 4)).toDouble();
-			item.fix_cc = model.data(model.index(i, 5)).toDouble();
-			item.fix_hb = model.data(model.index(i, 6)).toDouble();
-			item.fix_la = model.data(model.index(i, 7)).toDouble();
-			item.fix_ra = model.data(model.index(i, 8)).toDouble();
-			item.tBefore = model.data(model.index(i, 9)).toTime();
-			item.tAfter = model.data(model.index(i, 10)).toTime();
-			item.strTheaterName = model.data(model.index(i, 11)).toString();
+			item.fix_ftl = pModel->data(pModel->index(i, 2)).toDouble();
+			item.fix_lc = pModel->data(pModel->index(i, 3)).toDouble();
+			item.fix_rc = pModel->data(pModel->index(i, 4)).toDouble();
+			item.fix_cc = pModel->data(pModel->index(i, 5)).toDouble();
+			item.fix_hb = pModel->data(pModel->index(i, 6)).toDouble();
+			item.fix_la = pModel->data(pModel->index(i, 7)).toDouble();
+			item.fix_ra = pModel->data(pModel->index(i, 8)).toDouble();
+			item.tBefore = pModel->data(pModel->index(i, 9)).toTime();
+			item.tAfter = pModel->data(pModel->index(i, 10)).toTime();
+			item.strTheaterName = pModel->data(pModel->index(i, 11)).toString();
 			//item.mSyncMode = MODE_SIS;
-			item.mSyncMode = (MODESYNC)model.data(model.index(i, 13)).toInt();
-			item.strScript = model.data(model.index(i, 12)).toString();
+			item.mSyncMode = (MODESYNC)pModel->data(pModel->index(i, 13)).toInt();
+			item.strScript = pModel->data(pModel->index(i, 12)).toString();
 			if (item.strScript.isEmpty() || item.strScript == "")
 			{
 				item.strScript = "sleep=12000;\
@@ -199,6 +204,8 @@ void SQMSDatabase::GetQDevList(TQDevItemList& itemList)
 			}
 			itemList.push_back(item);
 		}
+		delete pModel;
+		pModel = NULL;
 		CloseDB();
 	}
 	nFlag = 0;
@@ -230,17 +237,22 @@ void SQMSDatabase::SaveCollectData(TValueItemList *pList, QMSDEVICE& mDev)
 		QString sql = s1 + s2 + "');";
 		sql.remove("\r");
 		sql.remove("\n");
-		model.setQuery(sql, db);
-		if (model.lastError().isValid())
+		pModel = new QSqlQueryModel;
+		pModel->setQuery(sql, db);
+		if (pModel->lastError().isValid())
 		{
 			pLog->Write(LOG_DATABASE, QObject::tr("Execute SaveCollectData SQL failure."));
 			pLog->Write(LOG_DATABASE, sql);
-			pLog->Write(LOG_DATABASE, model.lastError().text());
+			pLog->Write(LOG_DATABASE, pModel->lastError().text());
+			delete pModel;
+			pModel = NULL;
 			CloseDB();
 			nFlag = 0;
 			return;
 		}
 		pLog->Write(LOG_DATABASE, sql);
+		delete pModel;
+		pModel = NULL;
 		CloseDB();
 	}
 	nFlag = 0;
@@ -254,19 +266,22 @@ QSqlQueryModel* SQMSDatabase::ExecSql(QString sql)
 	OpenDB();
 	if (db.isOpen())
 	{
-		model.setQuery(sql, db);
-		if (model.lastError().isValid())
+		pModel = new QSqlQueryModel;
+		pModel->setQuery(sql, db);
+		if (pModel->lastError().isValid())
 		{
 			pLog->Write(LOG_DATABASE, QObject::tr("Execute ExecSql SQL failure."));
 			pLog->Write(LOG_DATABASE, sql);
-			pLog->Write(LOG_DATABASE, model.lastError().text());
+			pLog->Write(LOG_DATABASE, pModel->lastError().text());
+			delete pModel;
+			pModel = NULL;
 			CloseDB();
 			nFlag = 0;
 			return NULL;
 		}
 		CloseDB();
 		nFlag = 0;
-		return &model;
+		return pModel;
 	}
 	nFlag = 0;
 	return NULL;
